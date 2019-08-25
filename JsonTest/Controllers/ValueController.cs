e1eadb634f91200733cad230478b739d1ddfc680
Brecht Carlier <erazerbrecht@gmail.com>
2019-08-25 22:37:43 +0200

    Added repro

---
+++ JsonTest/Controllers/ValueController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JsonTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValueController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<ValueViewModel> Get()
+        {
+            var car = new Car
+            {
+                Speed = 120,
+                HorsePower = 130
+            };
+
+            var bus = new Bus
+            {
+                Speed = 90,
+                HumanCapacity = 50
+            };
+
+            var vm = new ValueViewModel
+            {
+                Transportations = new List<Transportation> { car, bus }
+            };
+
+            return Ok(vm);
+        }
+    }
+
+    public class ValueViewModel
+    {
+        public List<Transportation> Transportations { get; set; }
+    }
+
+    public abstract class Transportation
+    {
+        public int Speed { get; set; }
+    }
+
+    public class Car: Transportation
+    {
+        public int HorsePower { get; set; }
+    }
+
+    public class Bus: Transportation
+    {
+        public int HumanCapacity { get; set; }
+    }
+}
